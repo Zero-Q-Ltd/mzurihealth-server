ba39c4a1a2c2d1cf14eb79f40c24e71f983a5ab7
kamana@kisinga.family <kamana@kisinga.family>
2019-07-10 22:55:58 +0300

    added persistent logging to gin

--- server/server.go
+++ server/server.go
@@ -1,62 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/handler"
-	"github.com/go-chi/chi"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/kisinga/mzurihealth/gql/gen"
-	"github.com/rs/cors"
 )
+
+//	cors "github.com/rs/cors/wrapper/gin"
 
 const defaultPort = "4242"
 
 func main() {
-	router := chi.NewRouter()
+	router := gin.Default()
 
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
-	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:4200"},
-		AllowCredentials: true,
-		//All logs are recorded below
-		Debug: false,
-	}).Handler)
+	c := cors.Default()
+	router.Use(c)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-	logPath := "development.log"
-	openLogFile(logPath)
 
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	gin.DisableConsoleColor()
 
-	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	router.Handle("/query", handler.GraphQL(gen.NewExecutableSchema(gen.Config{Resolvers: &gen.Resolver{}})))
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	err := http.ListenAndServe(":"+port, logRequest(router))
+	//set up log file
+	logPath := "server.log"
+	logfile, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("Failed to create request log file:", err)
 	}
-}
 
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
-}
-func openLogFile(logfile string) {
-	if logfile != "" {
-		lf, err := os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+	// set request logging
+	gin.DefaultWriter = logfile
 
-		if err != nil {
-			log.Fatal("OpenLogfile: os.OpenFile:", err)
-		}
-		log.SetOutput(lf)
-	}
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	router.Use(gin.Recovery())
+
+	gql := gin.WrapH(handler.GraphQL(gen.NewExecutableSchema(gen.Config{Resolvers: &gen.Resolver{}})))
+	router.GET("/", gin.WrapH(handler.Playground("GraphQL playground", "/api")))
+	router.GET("/api", gql)
+	router.POST("/api", gql)
+	router.Run(":" + port)
+
 }
