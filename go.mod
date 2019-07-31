53fb9512b4a0c890bc9e1102a6a6a61cb5329b4e
kamana@kisinga.family <kamana@kisinga.family>
2019-07-31 06:43:50 +0300

    added static file hosting

--- go.mod
+++ go.mod
@@ -4,21 +4,31 @@ go 1.12
 
 require (
 	github.com/99designs/gqlgen v0.9.1
-	github.com/Bose/go-gin-opentracing v1.0.4
 	github.com/agnivade/levenshtein v1.0.2 // indirect
-	github.com/gin-contrib/cors v1.3.0
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.6
+	github.com/labstack/gommon v0.2.9
 	github.com/opentracing/opentracing-go v1.1.0
+	github.com/prometheus/procfs v0.0.0-20190104112138-b1a0a9a36d74 // indirect
 	github.com/tidwall/pretty v1.0.0 // indirect
-	github.com/uber/jaeger-client-go v2.16.0+incompatible // indirect
-	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	github.com/uber-go/atomic v1.3.2 // indirect
+	github.com/uber/jaeger-client-go v2.16.0+incompatible
+	github.com/uber/jaeger-lib v2.0.0+incompatible
+	github.com/urfave/cli v1.20.0
 	github.com/vektah/gqlparser v1.1.2
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
+	go.elastic.co/apm v1.4.0
+	go.elastic.co/apm/module/apmot v1.4.0
 	go.mongodb.org/mongo-driver v1.0.4
+	go.uber.org/atomic v1.3.2 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
+	sourcegraph.com/sourcegraph/appdash v0.0.0-20180110180208-2cc67fd64755
 )
--- server/server.go
+++ server/server.go
@@ -1,37 +1,238 @@
 package main
 
+// import (
+// 	"context"
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"path"
+// 	"path/filepath"
+
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/bson/primitive"
+
+// 	"github.com/99designs/gqlgen/handler"
+// 	"github.com/gin-contrib/cors"
+
+// 	"github.com/kisinga/mzurihealth/config"
+// 	"github.com/kisinga/mzurihealth/db"
+// 	"github.com/kisinga/mzurihealth/gql/gen"
+// 	"github.com/kisinga/mzurihealth/models"
+// )
+
+// //	cors "github.com/rs/cors/wrapper/gin"
+
+// const defaultPort = "4242"
+
+// func main() {
+
+// 	//Create the first context
+// 	ctx := context.Background()
+
+// 	//Create a connection to db
+// 	dberr := db.ConnectDB(ctx, "test")
+// 	if dberr != nil {
+// 		initError("ConnectDB", dberr)
+// 	}
+// 	//Dont fotget to close connection to db
+// 	defer db.CloseSession(ctx)
+
+// 	//Read the config first
+// 	hosp, configerr := config.ReadFile()
+// 	//Only create a new hospital if a config file does not exist
+// 	if configerr != nil {
+// 		fmt.Println("Error reading file")
+// 		clicommands(ctx)
+
+// 		// if configerr.Error() == "no such file or directory" {
+// 		// 	fmt.Println("File doesnt exist")
+// 		// 	clicommands(ctx)
+// 		// }
+// 	}
+// 	decodeerr := db.QueryDocument(ctx, "", "hospitals", bson.D{{"_id", hosp.ID}}).Decode(&hospital)
+// 	if decodeerr != nil {
+// 		initError("Decode Hospital After Reading from DB", decodeerr)
+// 	}
+// 	e := echo.New()
+
+// 	//
+// 	// p := tracer.InitTracing()
+// 	// tell gin to use the middleware
+// 	e.Use(p)
+
+// 	// Add CORS middleware around every request
+// 	// See https://github.com/rs/cors for full option listing
+// 	c := cors.Default()
+
+// 	// r.Use(auth.Middleware())
+
+// 	e.Use(c)
+
+// 	//This can vary according to client, in case they have something else running
+// 	//on port 4242
+// 	port := os.Getenv("MZURIHEALTH PORT")
+// 	if port == "" {
+// 		port = defaultPort
+// 	}
+
+// 	e.Use(gin.Recovery()) // add Recovery middleware
+
+// 	e.GET("/app", func(c *gin.Context) {
+// 		dir, file := path.Split(c.Request.RequestURI)
+// 		ext := filepath.Ext(file)
+// 		if file == "" || ext == "" {
+// 			c.File("./public/index.html")
+// 		} else {
+// 			// strings.Split(file, "?")
+// 			c.File("./public" + path.Join(dir, file))
+// 		}
+
+// 	})
+// 	e.POST("/api", graphqlHandler())
+// 	e.GET("/api", graphqlHandler())
+// 	e.GET("/", playgroundHandler())
+// 	_ = e.Run(":" + port)
+// }
 import (
 	"context"
-	"flag"
 	"fmt"
+	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 
 	"github.com/99designs/gqlgen/handler"
-	"github.com/gin-contrib/cors"
-
 	"github.com/kisinga/mzurihealth/config"
 	"github.com/kisinga/mzurihealth/db"
 	"github.com/kisinga/mzurihealth/gql/gen"
 	"github.com/kisinga/mzurihealth/models"
-	"github.com/kisinga/mzurihealth/tracer"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/urfave/cli"
 )
 
-//	cors "github.com/rs/cors/wrapper/gin"
-
-const defaultPort = "4242"
+const (
+	banner = `MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWK00XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkocclxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkollc:::lxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkollllc:::::lxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkollllllc:::::::lxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMWKkollllllllc:::::::::lxKWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMWKkollllllllllc:::::::::::lxKWMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWKkollllllllllllc:::::::::::::lxKWMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWKkolllllllllllloxxxoc::::::::::::lxKWMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMWXkollllllllllllox0XNNXOoc::::::::::::lxKWMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWKkollllllllllllox0XWWWWWNXOoc::::::::::::lxKWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMWKkollllllllllllox0XWWMMMMMWWNXOoc::::::::::::lkXWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMWKkollllllllllllox0XWWWMMMMMMMMWWNXOoc::::::::::::lxKWMMMMMMMMMMMMM
+MMMMMMMMMMMWKkollllllllllllox0XWWWMMMMMMMMMMMMWWNXOoc::::::::::::lxKWMMMMMMMMMMM
+MMMMMMMMMWKkollllllllllllldOKXNNNNNNNNNNNNNNNNNNNXX0xl:::::::::::::lkKWMMMMMMMMM
+MMMMMMMWXkolllllllllllllc:cccllllllllloollcccccccc:::;,;:::::::::::::lkXWMMMMMMM
+MMMMMWXkolllllllllllllc;,,,,,;;;;,,;:ccc::;,'''''''''''',;:::::::::::::lkXWMMMMM
+MMMWXkolllllllllllllc;,,,,,,,;;,;;:clllc:::;;,'''''''''''',;;::::::::::::lkXWMMM
+MMMNxlllllllllllllc;,,,,;;,,;,;;:clllllc:::::;;,'''''''''''',;;::::::::::::xNMMM
+MMMN0dllllllllllc:;,,,,,;;;;;;:clllllllc:::::::;;,'''''''''''',;:::::::::co0NWMM
+MMMWNXOdllllllc:;,,,;;;;;,;;:clllllllllc:::::::::;;,'''''''''''',;;::::coOXWWMMM
+MMMMWWNXOdllc:;,,,;;,;;;;;:clllllllllllc:::::::::::;;,'''''''''''',;:coOXNWWMMMM
+MMMMMMWWNXOxdddddddddddlcclllllllllllllcc::::::::::::;;:loooooooooooxOXNWWMMMMMM
+MMMMMMMMMWWNNNNNNNNNNKOdllllllllllllldkOkxl:::::::::::::okXNNNNNNNNNNWWWMMMMMMMM
+MMMMMMMMMMMMMMMMMMWXOdllllllllllllldOKNNNX0xl:::::::::::::oONWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMXxllllllllllllldOXNWWWWWWNKxl::::::::::::cxNMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWXkollllllllllclkXWWMMMMMMWWKd:;::::::::::cxXWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWWX0xollllllc:;,:dKWMMMMMMN0l,'',;::::::cdOXWWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMWWNXOdollc:;,,,,;:dKWWWN0o;'''''',;::cdOXNWWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMWWNXOdc:;;:;:::::lxOkd:,,,,,,''',:oOXNWWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMWWNXOdlllllllllllc:;;;;;;;;;;:okXNWWMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMWWNXOdollllllllc::;::;;;:coOXNWWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWNXOxollllllc:::::::cdOXWWWMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXOxollllc:::::cdOXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX0xollc:::cdOXWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXOxlc:cdOXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX0kk0XWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+`
+	timeoutInSeconds = "620s"
+)
 
 var hospital models.Hospital
+
+// version set by LDFLAGS
+var version string
+
+func start(ctx context.Context, root string, port int, redirectHttps bool, logFormat string) {
+
+	e := echo.New()
+	e.HideBanner = true
+
+	//Read the config first
+	hosp, configerr := config.ReadFile()
+	//Only create a new hospital if a config file does not exist
+	if configerr != nil {
+		fmt.Println("Error reading file")
+
+	}
+
+	decodeerr := db.QueryDocument(ctx, "", "hospitals", bson.D{{"_id", hosp.ID}}).Decode(&hospital)
+	if decodeerr != nil {
+		initError("Decode Hospital After Reading from DB", decodeerr)
+	}
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: logFormat,
+	}))
+
+	if redirectHttps {
+		e.Pre(middleware.HTTPSRedirect())
+	}
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:   root,
+		Index:  "index.html",
+		HTML5:  true,
+		Browse: false,
+	}))
+	timeOut, _ := time.ParseDuration(timeoutInSeconds)
+	e.Server.IdleTimeout = timeOut
+	graphqlHandler := handler.GraphQL(gen.NewExecutableSchema(gen.Config{Resolvers: &gen.Resolver{}}))
+	playgroundHandler := handler.Playground("GraphQL", "/api")
+
+	// Middleware
+	// e.Use(Process)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+	e.POST("/api", func(c echo.Context) error {
+		req := c.Request()
+		res := c.Response()
+		graphqlHandler.ServeHTTP(res, req)
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		req := c.Request()
+		res := c.Response()
+		playgroundHandler.ServeHTTP(res, req)
+		return nil
+	})
+
+	fmt.Printf(banner, "v"+version)
+	fmt.Printf("» http server started on port %d\n", port)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+}
 
 func main() {
 
 	//Create the first context
 	ctx := context.Background()
-
 	//Create a connection to db
 	dberr := db.ConnectDB(ctx, "test")
 	if dberr != nil {
@@ -40,114 +241,114 @@ func main() {
 	//Dont fotget to close connection to db
 	defer db.CloseSession(ctx)
 
-	//Read the config first
-	hosp, configerr := config.ReadFile()
-	//Only create a new hospital if a config file does not exist
-	if configerr != nil {
-		fmt.Println("Error reading file")
-		clicommands(ctx)
+	app := cli.NewApp()
+	app.Name = "Mzurihealth"
+	app.Usage = "Mzurihealth server"
+	app.Version = version
 
-		// if configerr.Error() == "no such file or directory" {
-		// 	fmt.Println("File doesnt exist")
-		// 	clicommands(ctx)
-		// }
+	app.Flags = []cli.Flag{}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "start",
+			Usage: "Start the SPA server",
+			Action: func(c *cli.Context) error {
+				start(ctx, c.String("dir"), c.Int("port"), c.Bool("https-redirect"), c.String("log-format")+"\n")
+				return nil
+			},
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "dir, d",
+					Value:  "./dist",
+					Usage:  "Directory from which to serve files.",
+					EnvVar: "MZURIHEALTH_DIR",
+				},
+				cli.StringFlag{
+					Name:   "port, p",
+					Value:  "8080",
+					Usage:  "Server port.",
+					EnvVar: "MZURIHEALTH_PORT",
+				},
+				cli.BoolFlag{
+					Name:   "https-redirect",
+					Usage:  "Use to force http to redirect to https",
+					EnvVar: "MZURIHEALTH_HTTPS_REDIRECT",
+				},
+				cli.StringFlag{
+					Name: "log-format",
+					Value: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
+						`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
+						`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
+						`"bytes_out":${bytes_out}}`,
+					Usage:  "Specify the log format",
+					EnvVar: "MZURIHEALTH_LOG_FORMAT",
+				},
+			},
+		},
+		{
+			Name:  "new",
+			Usage: "Create a new hospital",
+			Action: func(c *cli.Context) error {
+				createhospital(c.String("name"))
+				return nil
+			},
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "name, n",
+					Value: "",
+					Usage: "Name of the new hospital",
+				},
+			},
+		},
 	}
-	decodeerr := db.QueryDocument(ctx, "", "hospitals", bson.D{{"_id", hosp.ID}}).Decode(&hospital)
-	if decodeerr != nil {
-		initError("Decode Hospital After Reading from DB", decodeerr)
-	}
-	r := gin.Default()
-	gin.SetMode(gin.DebugMode)
 
-	//
-	p := tracer.InitTracing()
-	// tell gin to use the middleware
-	r.Use(p)
-
-	// Add CORS middleware around every request
-	// See https://github.com/rs/cors for full option listing
-	c := cors.Default()
-
-	// r.Use(auth.Middleware())
-
-	r.Use(c)
-
-	//This can vary according to client, in case they have something else running
-	//on port 4242
-	port := os.Getenv("MZURIHEALTH PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	r.Use(gin.Recovery()) // add Recovery middleware
-
-	r.POST("/api", graphqlHandler())
-	r.GET("/api", graphqlHandler())
-	r.GET("/", playgroundHandler())
-	_ = r.Run(":" + port)
+	// start()
+	app.Run(os.Args)
 }
-
-func clicommands(ctx context.Context, cmd ...string) {
-	fmt.Println("Checking cli options...")
-	newindicator := flag.Bool("new", false, "specify name to create a new hospital")
-	name := flag.String("name", "", "specify name to create a new hospital")
-	flag.Parse()
-
-	if *newindicator {
-		/**
-		*Make changnes before marshalling to take advantage of linter and avoid runtime errors
-		**/
-		hospital.Name = *name
-		var newhosp bson.M
-		b, _ := bson.Marshal(hospital)
-		bson.Unmarshal([]byte(b), &newhosp)
-
-		res, err := db.InserDocument(ctx, "", "hospitals", "", newhosp)
-		if err != nil {
-			initError("Create Hospital", err)
-		}
-		str, ok := res.InsertedID.(primitive.ObjectID)
-		if ok {
-			fmt.Printf("ID is: %q\n", str.Hex())
-		} else {
-			fmt.Printf("value is not a string\n")
-		}
-		objID, _ := primitive.ObjectIDFromHex(str.Hex())
-		result := db.QueryDocument(ctx, "", "hospitals", bson.D{{"_id", objID}})
-		var temp models.Hospital
-		var decodeerr = result.Decode(&temp)
-		if decodeerr != nil {
-			initError("Decode Hospital", decodeerr)
-		}
-		hospital = temp
-		createerr := config.Create(hospital)
-		if createerr != nil {
-			initError("Error creating File", createerr)
-		}
-		return
-	} else {
-		initError("No Cli commands", nil)
+func createhospital(hospitalname string) {
+	/**
+	*Make changnes before marshalling to take advantage of linter and avoid runtime errors
+	**/
+	//Read the config first
+	_, configerr := config.ReadFile()
+	//Only create a new hospital if a config file does not exist
+	if configerr == nil {
+		fmt.Println("Config File already exists")
+		initError("Create Hospital", nil)
 	}
+	var ctx = context.Background()
+	hospital.Name = hospitalname
+	var newhosp bson.M
+	b, _ := bson.Marshal(hospital)
+	bson.Unmarshal([]byte(b), &newhosp)
+
+	res, err := db.InserDocument(ctx, "", "hospitals", "", newhosp)
+	if err != nil {
+		initError("Create Hospital", err)
+	}
+	str, ok := res.InsertedID.(primitive.ObjectID)
+	if ok {
+		fmt.Printf("ID is: %q\n", str.Hex())
+	} else {
+		fmt.Printf("value is not a string\n")
+	}
+	objID, _ := primitive.ObjectIDFromHex(str.Hex())
+	result := db.QueryDocument(ctx, "", "hospitals", bson.D{{"_id", objID}})
+	var temp models.Hospital
+	var decodeerr = result.Decode(&temp)
+	if decodeerr != nil {
+		initError("Decode Hospital", decodeerr)
+	}
+	hospital = temp
+	createerr := config.Create(hospital)
+	if createerr != nil {
+		initError("Error creating File", createerr)
+	}
+	fmt.Printf("Success creating hospital: %q\n", hospitalname)
 }
 
 func initError(function string, e error) {
 	fmt.Println(function + " Init Error:")
 	fmt.Print(e)
 	panic("Init Error")
-}
-
-// Defining the Graphql handler
-func graphqlHandler() gin.HandlerFunc {
-	h := handler.GraphQL(gen.NewExecutableSchema(gen.Config{Resolvers: &gen.Resolver{}}))
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
-// Defining the Playground handler
-func playgroundHandler() gin.HandlerFunc {
-	h := handler.Playground("GraphQL", "/api")
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
 }
